5adaae534e1b0327949c824d3a8ab7184e877a56
Pankaj Kumawat <pkumawat@Pankajs-MacBook-Pro.local>
2020-10-05 12:15:12 +0530

    logo and anv

--- public/js/components/Nav.jsx
+++ public/js/components/Nav.jsx
@@ -2,22 +2,24 @@ const quiteRedirect = (event) => {
     event.preventDefault();
     silentUrlChangeTo(event.target.href);
 }
+
 const Nav = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
             <div className="container">
-                <a className="navbar-brand" href="#">
-                    <img src="http://placehold.it/150x50?text=Logo" alt=""/>
+                <a className="navbar-brand" href={VALID_PATHS.HOME} onClick={quiteRedirect}>
+                    <div className="logo" style={{display: "inline"}}></div>
+                    qlinks
                 </a>
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/home" onClick={quiteRedirect}>
+                        <a className="nav-link" href={VALID_PATHS.HOME} onClick={quiteRedirect}>
                             Home
                             <span className="sr-only">(current)</span>
                         </a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/logout" onClick={props.logout}>Logout</a>
+                        <a className="nav-link" href={VALID_PATHS.LOGOUT} onClick={props.logout}>Logout</a>
                     </li>
                 </ul>
             </div>
